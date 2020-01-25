10a63e6ff9063abded0d913b005e060e7c497966
jonathan e godinez <jongodinezbass@yahoo.com>
2020-01-25 11:58:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,15 +1,17 @@
-def square_array(array)
+#def square_array(array)
     #square_array = [numbers]
-    numbers = Array.new
+   # numbers = Array.new
    # square_array.each do |number|
-   numbers.each do |number|
-    numbers[count] << (number**2)
+   #numbers.each do |number|
+    #numbers[count] << (number**2)
     
-    end
-  end 
+    #end
+  #end 
+  
+  
   def square_array(numbers)
-  # your code here	  new_numbers = []
-end 	  numbers.each do |num|
+    new_numbers = []
+ 	  numbers.each do |num|
     new_numbers << (num ** 2)
   end
   return new_numbers
