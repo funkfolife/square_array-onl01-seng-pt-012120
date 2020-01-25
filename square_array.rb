7beea61939f7e528fec67750e6f5650817934bb9
jonathan e godinez <jongodinezbass@yahoo.com>
2020-01-25 11:52:03 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,10 +7,3 @@ def square_array(array)
     end
   end 
   
-# def square_array(numbers)
- # count = 0
-  #while count < numbers.length do
-   # numbers.index ** 2 
-  #end
-  #square_array(numbers)
-#end 
