fdf7570e5d4a1227deb4142263da6362a7cb7150
jonathan e godinez <jongodinezbass@yahoo.com>
2020-01-25 11:35:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,11 @@
 def square_array(numbers)
     #square_array = [numbers]
     count = 0 
-    square_array.each do |number|
+   # square_array.each do |number|
+   while count < numbers.length do 
     square_array[count] = number
     square_array=Array.new 
-    sqare_array
+    sqare_array << (number**2)
     count+=1 
     end 
   end 
