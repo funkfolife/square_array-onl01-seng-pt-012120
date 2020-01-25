7c0abaebcbfdbf054f7bc0983a492d5088fc3972
jonathan e godinez <jongodinezbass@yahoo.com>
2020-01-25 12:02:41 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,18 +1,9 @@
-def square_array(numbers)
-    #square_array = []
-   # numbers = Array.new
-   # square_array.each do |number|
-    numbers.each do |number|
-    numbers[count] << (number**2)
-    end
-    return squre_array 
-  end 
+
   
-  
- # def square_array(numbers)
- #   new_numbers = []
-# 	  numbers.each do |num|
- #   new_numbers << (num ** 2)
-#  end
- # return new_numbers
-#end
+  def square_array(numbers)
+    new_numbers = []
+ 	  numbers.each do |num|
+    new_numbers << (num ** 2)
+  end
+  return new_numbers
+end
