f88a60a25c93cd2d10e9d1b3108a86280135c753
jonathan e godinez <jongodinezbass@yahoo.com>
2020-01-25 11:32:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
-def square_array(array)
-  # your code here
-end
+def square_array(numbers)
+    #square_array = [numbers]
+    count = 0 
+    square_array.each do |number|
+    square_array[count] = number
+    square_array=Array.new 
+    sqare_array.push
+    count+=1 
+    end 
+  end 
+  
+# def square_array(numbers)
+ # count = 0
+  #while count < numbers.length do
+   # numbers.index ** 2 
+  #end
+  #square_array(numbers)
+#end 
